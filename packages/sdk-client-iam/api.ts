f97fa5f7b1ac465d8d318cddd740b45cfd0efa1e
Marat Shakirov <marat.s@affinidi.com>
2024-02-07 15:32:29 +0100

    feat: update client iam

--- packages/sdk-client-iam/api.ts
+++ packages/sdk-client-iam/api.ts
@@ -1313,6 +1313,137 @@ export class ConsumerAuthApi extends BaseAPI {
 }
 
 /**
+ * CorsApi - axios parameter creator
+ * @export
+ */
+export const CorsApiAxiosParamCreator = function (
+  configuration?: Configuration
+) {
+  return {
+    /**
+     * Enable CORS by returning correct headers
+     * @summary CORS Support
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    corsConsumerAuthTokenEndpoint: async (
+      options: RawAxiosRequestConfig = {}
+    ): Promise<RequestArgs> => {
+      const localVarPath = `/v1/consumer/oauth2/token`;
+      // use dummy base URL string because the URL constructor only accepts absolute URLs.
+      const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+      let baseOptions;
+      if (configuration) {
+        baseOptions = configuration.baseOptions;
+      }
+
+      const localVarRequestOptions = {
+        method: 'OPTIONS',
+        ...baseOptions,
+        ...options,
+      };
+      const localVarHeaderParameter = {} as any;
+      const localVarQueryParameter = {} as any;
+
+      setSearchParams(localVarUrlObj, localVarQueryParameter);
+      let headersFromBaseOptions =
+        baseOptions && baseOptions.headers ? baseOptions.headers : {};
+      localVarRequestOptions.headers = {
+        ...localVarHeaderParameter,
+        ...headersFromBaseOptions,
+        ...options.headers,
+      };
+
+      return {
+        url: toPathString(localVarUrlObj),
+        options: localVarRequestOptions,
+      };
+    },
+  };
+};
+
+/**
+ * CorsApi - functional programming interface
+ * @export
+ */
+export const CorsApiFp = function (configuration?: Configuration) {
+  const localVarAxiosParamCreator = CorsApiAxiosParamCreator(configuration);
+  return {
+    /**
+     * Enable CORS by returning correct headers
+     * @summary CORS Support
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    async corsConsumerAuthTokenEndpoint(
+      options?: RawAxiosRequestConfig
+    ): Promise<
+      (axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>
+    > {
+      const localVarAxiosArgs =
+        await localVarAxiosParamCreator.corsConsumerAuthTokenEndpoint(options);
+      const index = configuration?.serverIndex ?? 0;
+      const operationBasePath =
+        operationServerMap['CorsApi.corsConsumerAuthTokenEndpoint']?.[index]
+          ?.url;
+      return (axios, basePath) =>
+        createRequestFunction(
+          localVarAxiosArgs,
+          globalAxios,
+          BASE_PATH,
+          configuration
+        )(axios, operationBasePath || basePath);
+    },
+  };
+};
+
+/**
+ * CorsApi - factory interface
+ * @export
+ */
+export const CorsApiFactory = function (
+  configuration?: Configuration,
+  basePath?: string,
+  axios?: AxiosInstance
+) {
+  const localVarFp = CorsApiFp(configuration);
+  return {
+    /**
+     * Enable CORS by returning correct headers
+     * @summary CORS Support
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    corsConsumerAuthTokenEndpoint(options?: any): AxiosPromise<void> {
+      return localVarFp
+        .corsConsumerAuthTokenEndpoint(options)
+        .then((request) => request(axios, basePath));
+    },
+  };
+};
+
+/**
+ * CorsApi - object-oriented interface
+ * @export
+ * @class CorsApi
+ * @extends {BaseAPI}
+ */
+export class CorsApi extends BaseAPI {
+  /**
+   * Enable CORS by returning correct headers
+   * @summary CORS Support
+   * @param {*} [options] Override http request option.
+   * @throws {RequiredError}
+   * @memberof CorsApi
+   */
+  public corsConsumerAuthTokenEndpoint(options?: RawAxiosRequestConfig) {
+    return CorsApiFp(this.configuration)
+      .corsConsumerAuthTokenEndpoint(options)
+      .then((request) => request(this.axios, this.basePath));
+  }
+}
+
+/**
  * DefaultApi - axios parameter creator
  * @export
  */
