c296ec04497b0f5720e0ca7786fdbe659ef62734
Roman Brazhnyk <roman.b@affinidi.com>
2024-06-04 20:03:45 +0800

    feat: we have a single vault link for all platforms

--- libs/iota-browser/src/helpers/vault-handler.ts
+++ libs/iota-browser/src/helpers/vault-handler.ts
@@ -1,16 +1,16 @@
-import { WindowCache } from "./window-cache";
+import { WindowCache } from './window-cache'
 
 export class VaultHandler {
-    private cache = new WindowCache();
+  private cache = new WindowCache()
 
-    public openVault(id: string, link: string, window: Window) {
-        const win = window.open(link); // open - opens in a new window
-        this.cache.addWindow(id, win);
-    }
+  public openVault(id: string, link: string, window: Window) {
+    const win = window.open(link) // open - opens in a new window
+    this.cache.addWindow(id, win)
+  }
 
-    public closeVault(id: string) {
-        const win = this.cache.getWindow(id);
-        if (win) win.close();
-        this.cache.deleteWindow(id);
-    }
+  public closeVault(id: string) {
+    const win = this.cache.getWindow(id)
+    if (win) win.close()
+    this.cache.deleteWindow(id)
+  }
 }
