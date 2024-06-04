2c5e96a10b3e3d1a9442ba8f652322280c597a3f
Roman Brazhnyk <roman.b@affinidi.com>
2024-06-04 21:51:11 +0800

    feat: we have a single vault link for all platforms (#94)

--- packages/iota-utils/iota-utils.ts
+++ packages/iota-utils/iota-utils.ts
@@ -1,31 +1,20 @@
 import { EnvironmentHandler } from './helpers'
 import { Environment } from './helpers/environment'
 
-export interface IAffinidiVaultLinks {
-  web: string
-  android: string
-  ios: string
-}
-
-const LOCAL_WEB_VAULT_URL = 'http://localhost:3001/login'
-const DEV_WEB_VAULT_URL = 'https://vault.dev.affinidi.com/login'
-const PROD_WEB_VAULT_URL = 'https://vault.affinidi.com/login'
+const LOCAL_VAULT_URL = 'http://localhost:3001/login'
+const DEV_VAULT_URL = 'https://vault.dev.affinidi.com/login'
+const PROD_VAULT_URL = 'https://vault.affinidi.com/login'
 
 const envToWebVaultUrlMap = {
-  [Environment.LOCAL]: LOCAL_WEB_VAULT_URL,
-  [Environment.DEVELOPMENT]: DEV_WEB_VAULT_URL,
-  [Environment.PRODUCTION]: PROD_WEB_VAULT_URL,
+  [Environment.LOCAL]: LOCAL_VAULT_URL,
+  [Environment.DEVELOPMENT]: DEV_VAULT_URL,
+  [Environment.PRODUCTION]: PROD_VAULT_URL,
 }
-
-const ANDROID_VAULT_URL = 'affinidi-vault//something'
-const IOS_VAULT_URL = 'affinidi-vault//something'
 
 export class IotaUtils {
   static env = new EnvironmentHandler().fetchEnvironment()
 
-  static createVaultLinks(request: {
-    [key: string]: any
-  }): IAffinidiVaultLinks {
+  static createVaultLink(request: { [key: string]: any }): string {
     if (!request.payload.request || !request.payload.client_id) {
       throw Error('Please provide request and client_id in payload')
     }
@@ -34,11 +23,8 @@ export class IotaUtils {
     params.append('client_id', request.payload.client_id)
 
     const queryString = params.toString()
+    const vaultLink = `${envToWebVaultUrlMap[this.env] || PROD_VAULT_URL}?${queryString}`
 
-    return {
-      web: `${envToWebVaultUrlMap[this.env] || PROD_WEB_VAULT_URL}?${queryString}`,
-      android: `${ANDROID_VAULT_URL}?${queryString}`,
-      ios: `${IOS_VAULT_URL}?${queryString}`,
-    }
+    return vaultLink
   }
 }
