66e8c4d9e02754338367f7218a46a595be7bee8c
Carlos Rincon <carlos.r@affinidi.com>
2024-06-10 12:40:20 +0200

    fix: bundle type dependencies

--- packages/auth-provider/helpers/index.ts
+++ packages/auth-provider/helpers/index.ts
@@ -1,7 +1,7 @@
-export { IValidateToken, Jwt, ISignPayload } from './jwt'
-export { BffHeaders } from './get-bff-headers'
 export {
-  IFetchProjectScopedToken,
   ProjectScopedToken,
+  type IFetchProjectScopedToken,
 } from './fetch-project-scoped-token'
+export { BffHeaders } from './get-bff-headers'
 export { Iota } from './iota'
+export { Jwt, type ISignPayload, type IValidateToken } from './jwt'
