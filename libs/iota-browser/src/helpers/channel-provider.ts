f20e36e0085e7a9d3d08e2466d4ad77cc7e74d76
Pulkit Batra <pulkit.b@affinidi.com>
2024-06-14 12:59:31 +0530

    fix: add new iota client

--- libs/iota-browser/src/helpers/channel-provider.ts
+++ libs/iota-browser/src/helpers/channel-provider.ts
@@ -82,9 +82,9 @@ export class ChannelProvider {
     )
     this.iotaConfigBuilder = await this.createConfigBuilder(
       credentialsProvider,
-      credentials.iotClientId,
+      credentials.connectionClientId,
     )
-    this.topicName = `iota/v1/${credentials.iotClientId}`
+    this.topicName = `iota/v1/${credentials.connectionClientId}`
     this.iotaClient = await this.startClient()
   }
 
