27b8604becec4be3c724f147800f2cf68d484e0d
Marat Shakirov <marat.s@affinidi.com>
2024-09-17 22:22:14 +0000

    feat: update client all

--- clients/dart/iota_client/lib/model/update_configuration_by_id_input.dart
+++ clients/dart/iota_client/lib/model/update_configuration_by_id_input.dart
@@ -21,8 +21,7 @@ class UpdateConfigurationByIdInput {
     this.tokenMaxAge,
     this.description,
     this.clientMetadata,
-    this.mode,
-    this.redirectUri,
+    this.redirectUris = const [],
   });
 
   /// The name of the config
@@ -94,17 +93,8 @@ class UpdateConfigurationByIdInput {
   ///
   IotaConfigurationDtoClientMetadata? clientMetadata;
 
-  /// indicates whether the flow is a WebSocket flow or a Redirect flow. This value is used in Vault to determine how to process the data flow request.
-  UpdateConfigurationByIdInputModeEnum? mode;
-
-  /// the URL that the user will be redirected to after the request has been processed; should be provided by the developer of the client application. Required only if mode is Redirect.
-  ///
-  /// Please note: This property should have been non-nullable! Since the specification file
-  /// does not include a default value (using the "default:" property), however, the generated
-  /// source code must fall back to having a nullable type.
-  /// Consider adding a "default:" property in the specification file to hide this note.
-  ///
-  String? redirectUri;
+  /// the URL that the user will be redirected to after the request has been processed; should be provided by the developer of the client application.Required only if mode is Redirect.
+  List<String> redirectUris;
 
   @override
   bool operator ==(Object other) => identical(this, other) || other is UpdateConfigurationByIdInput &&
@@ -116,8 +106,7 @@ class UpdateConfigurationByIdInput {
     other.tokenMaxAge == tokenMaxAge &&
     other.description == description &&
     other.clientMetadata == clientMetadata &&
-    other.mode == mode &&
-    other.redirectUri == redirectUri;
+    _deepEquality.equals(other.redirectUris, redirectUris);
 
   @override
   int get hashCode =>
@@ -130,11 +119,10 @@ class UpdateConfigurationByIdInput {
     (tokenMaxAge == null ? 0 : tokenMaxAge!.hashCode) +
     (description == null ? 0 : description!.hashCode) +
     (clientMetadata == null ? 0 : clientMetadata!.hashCode) +
-    (mode == null ? 0 : mode!.hashCode) +
-    (redirectUri == null ? 0 : redirectUri!.hashCode);
+    (redirectUris.hashCode);
 
   @override
-  String toString() => 'UpdateConfigurationByIdInput[name=$name, walletAri=$walletAri, iotaResponseWebhookURL=$iotaResponseWebhookURL, enableVerification=$enableVerification, enableConsentAuditLog=$enableConsentAuditLog, tokenMaxAge=$tokenMaxAge, description=$description, clientMetadata=$clientMetadata, mode=$mode, redirectUri=$redirectUri]';
+  String toString() => 'UpdateConfigurationByIdInput[name=$name, walletAri=$walletAri, iotaResponseWebhookURL=$iotaResponseWebhookURL, enableVerification=$enableVerification, enableConsentAuditLog=$enableConsentAuditLog, tokenMaxAge=$tokenMaxAge, description=$description, clientMetadata=$clientMetadata, redirectUris=$redirectUris]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
@@ -178,16 +166,7 @@ class UpdateConfigurationByIdInput {
     } else {
       json[r'clientMetadata'] = null;
     }
-    if (this.mode != null) {
-      json[r'mode'] = this.mode;
-    } else {
-      json[r'mode'] = null;
-    }
-    if (this.redirectUri != null) {
-      json[r'redirectUri'] = this.redirectUri;
-    } else {
-      json[r'redirectUri'] = null;
-    }
+      json[r'redirectUris'] = this.redirectUris;
     return json;
   }
 
@@ -218,8 +197,9 @@ class UpdateConfigurationByIdInput {
         tokenMaxAge: mapValueOfType<int>(json, r'tokenMaxAge'),
         description: mapValueOfType<String>(json, r'description'),
         clientMetadata: IotaConfigurationDtoClientMetadata.fromJson(json[r'clientMetadata']),
-        mode: UpdateConfigurationByIdInputModeEnum.fromJson(json[r'mode']),
-        redirectUri: mapValueOfType<String>(json, r'redirectUri'),
+        redirectUris: json[r'redirectUris'] is Iterable
+            ? (json[r'redirectUris'] as Iterable).cast<String>().toList(growable: false)
+            : const [],
       );
     }
     return null;
@@ -269,78 +249,4 @@ class UpdateConfigurationByIdInput {
   static const requiredKeys = <String>{
   };
 }
-
-/// indicates whether the flow is a WebSocket flow or a Redirect flow. This value is used in Vault to determine how to process the data flow request.
-class UpdateConfigurationByIdInputModeEnum {
-  /// Instantiate a new enum with the provided [value].
-  const UpdateConfigurationByIdInputModeEnum._(this.value);
-
-  /// The underlying value of this enum member.
-  final String value;
-
-  @override
-  String toString() => value;
-
-  String toJson() => value;
-
-  static const redirect = UpdateConfigurationByIdInputModeEnum._(r'redirect');
-  static const websocket = UpdateConfigurationByIdInputModeEnum._(r'websocket');
-
-  /// List of all possible values in this [enum][UpdateConfigurationByIdInputModeEnum].
-  static const values = <UpdateConfigurationByIdInputModeEnum>[
-    redirect,
-    websocket,
-  ];
-
-  static UpdateConfigurationByIdInputModeEnum? fromJson(dynamic value) => UpdateConfigurationByIdInputModeEnumTypeTransformer().decode(value);
-
-  static List<UpdateConfigurationByIdInputModeEnum> listFromJson(dynamic json, {bool growable = false,}) {
-    final result = <UpdateConfigurationByIdInputModeEnum>[];
-    if (json is List && json.isNotEmpty) {
-      for (final row in json) {
-        final value = UpdateConfigurationByIdInputModeEnum.fromJson(row);
-        if (value != null) {
-          result.add(value);
-        }
-      }
-    }
-    return result.toList(growable: growable);
-  }
-}
-
-/// Transformation class that can [encode] an instance of [UpdateConfigurationByIdInputModeEnum] to String,
-/// and [decode] dynamic data back to [UpdateConfigurationByIdInputModeEnum].
-class UpdateConfigurationByIdInputModeEnumTypeTransformer {
-  factory UpdateConfigurationByIdInputModeEnumTypeTransformer() => _instance ??= const UpdateConfigurationByIdInputModeEnumTypeTransformer._();
-
-  const UpdateConfigurationByIdInputModeEnumTypeTransformer._();
-
-  String encode(UpdateConfigurationByIdInputModeEnum data) => data.value;
-
-  /// Decodes a [dynamic value][data] to a UpdateConfigurationByIdInputModeEnum.
-  ///
-  /// If [allowNull] is true and the [dynamic value][data] cannot be decoded successfully,
-  /// then null is returned. However, if [allowNull] is false and the [dynamic value][data]
-  /// cannot be decoded successfully, then an [UnimplementedError] is thrown.
-  ///
-  /// The [allowNull] is very handy when an API changes and a new enum value is added or removed,
-  /// and users are still using an old app with the old code.
-  UpdateConfigurationByIdInputModeEnum? decode(dynamic data, {bool allowNull = true}) {
-    if (data != null) {
-      switch (data) {
-        case r'redirect': return UpdateConfigurationByIdInputModeEnum.redirect;
-        case r'websocket': return UpdateConfigurationByIdInputModeEnum.websocket;
-        default:
-          if (!allowNull) {
-            throw ArgumentError('Unknown enum value to decode: $data');
-          }
-      }
-    }
-    return null;
-  }
-
-  /// Singleton [UpdateConfigurationByIdInputModeEnumTypeTransformer] instance.
-  static UpdateConfigurationByIdInputModeEnumTypeTransformer? _instance;
-}
-
 
