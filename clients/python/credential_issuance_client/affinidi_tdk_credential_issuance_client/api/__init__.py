40e1d0a42b90e0e2b33dbd5223586cf9d7d4dbd9
Akash Singha <akash.s@affinidi.com>
2024-12-04 10:52:24 +0000

    feat: update client cis

--- clients/python/credential_issuance_client/affinidi_tdk_credential_issuance_client/api/__init__.py
+++ clients/python/credential_issuance_client/affinidi_tdk_credential_issuance_client/api/__init__.py
@@ -3,6 +3,7 @@
 # import apis into api package
 from affinidi_tdk_credential_issuance_client.api.configuration_api import ConfigurationApi
 from affinidi_tdk_credential_issuance_client.api.credentials_api import CredentialsApi
+from affinidi_tdk_credential_issuance_client.api.default_api import DefaultApi
 from affinidi_tdk_credential_issuance_client.api.issuance_api import IssuanceApi
 from affinidi_tdk_credential_issuance_client.api.offer_api import OfferApi
 from affinidi_tdk_credential_issuance_client.api.well_known_api import WellKnownApi
