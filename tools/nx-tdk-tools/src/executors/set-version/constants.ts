24eb512a03a1440d79c91e0a607e9ae4a3a88487
Marat Shakirov <marat.s@affinidi.com>
2025-01-08 17:47:31 +0100

    fix: apply native formatting

--- tools/nx-tdk-tools/src/executors/set-version/constants.ts
+++ tools/nx-tdk-tools/src/executors/set-version/constants.ts
@@ -1,4 +1,4 @@
-export const PYTHON_CLIENTS_PATH = "clients/python";
-export const TS_CLIENTS_PATH = "clients/typescript";
-export const DART_CLIENTS_PATH = "clients/dart";
-export const JAVA_CLIENTS_PATH = "clients/java";
+export const PYTHON_CLIENTS_PATH = 'clients/python'
+export const TS_CLIENTS_PATH = 'clients/typescript'
+export const DART_CLIENTS_PATH = 'clients/dart'
+export const JAVA_CLIENTS_PATH = 'clients/java'
--- tools/nx-tdk-tools/src/executors/set-version/types.ts
+++ tools/nx-tdk-tools/src/executors/set-version/types.ts
@@ -1,10 +1,10 @@
 export enum Language {
-	python = "python",
-	dart = "dart",
-	java = "java",
+  python = 'python',
+  dart = 'dart',
+  java = 'java',
 }
 
 export interface SetVersionExecutorOptions {
-	packageName: string;
-	language: Language;
+  packageName: string
+  language: Language
 }
