5e3794ffdcaf897b1283674f79a13eb2a727e381
Earl Grant Reyes <earl.r@affinidi.com>
2025-01-14 15:42:48 +0800

    fix: removed old file

--- packages/dart/affinidi_cryptography_service/lib/affinidi_cryptography_service.dart
+++ packages/dart/affinidi_cryptography_service/lib/affinidi_cryptography_service.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/affinidi_cryptography_service_base.dart';
+export 'src/affinidi_cryptography_service.dart';
 
 // TODO: Export any libraries intended for clients of this package.
