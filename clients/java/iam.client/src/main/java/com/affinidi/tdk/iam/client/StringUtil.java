289198d7ce3f20e02e2bd20866599a1f77b2d856
Marat Shakirov <marat.s@affinidi.com>
2025-01-15 15:11:13 +0100

    feat: update client iam

--- clients/java/iam.client/src/main/java/com/affinidi/tdk/iam/client/StringUtil.java
+++ clients/java/iam.client/src/main/java/com/affinidi/tdk/iam/client/StringUtil.java
@@ -16,7 +16,7 @@ package com.affinidi.tdk.iam.client;
 import java.util.Collection;
 import java.util.Iterator;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-01-13T09:21:47.313409253Z[Etc/UTC]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.9.0")
 public class StringUtil {
   /**
    * Check if the given array contains the given value (with case-insensitive comparison).
--- clients/java/iam.client/src/main/java/com/affinidi/tdk/iam/client/models/CreateProjectInput.java
+++ clients/java/iam.client/src/main/java/com/affinidi/tdk/iam/client/models/CreateProjectInput.java
@@ -33,7 +33,7 @@ import java.util.StringJoiner;
   CreateProjectInput.JSON_PROPERTY_NAME,
   CreateProjectInput.JSON_PROPERTY_DESCRIPTION
 })
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-01-13T09:21:47.313409253Z[Etc/UTC]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.9.0")
 public class CreateProjectInput {
   public static final String JSON_PROPERTY_NAME = "name";
   private String name;
