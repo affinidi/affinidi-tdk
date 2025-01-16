566aaa1044858ac3fb9b813876b217a8c1810e54
Atsuhiro Endo <atsuhiro.e@affinidi.com>
2025-01-16 16:57:43 +0100

    refactor: add nested property with name

--- packages/java/auth.provider/src/test/java/com/affinidi/tdk/authProvider/AuthProviderTest.java
+++ packages/java/auth.provider/src/test/java/com/affinidi/tdk/authProvider/AuthProviderTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -13,34 +14,44 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class AuthProviderTest {
 
-    @Test
-    void testAuthProviderConfiguration() throws Exception {
-        AuthProvider provider = new AuthProvider.Configurations()
-                .projectId("test-project")
-                .tokenId("test-token")
-                .privateKey("test-key")
-                .build();
-
-        assertNotNull(provider);
-        assertEquals("test-project", provider.getProjectId());
-    }
-
-    @ParameterizedTest
-    @DisplayName("throws given an invalid private-key and a empty or non-empty passphrase")
-    @EmptySource
-    @ValueSource(strings = { "complicated-word" })
-    void testInvalidPrivateKey(String phrase) {
-        Exception exception = assertThrows(Exception.class, () -> {
+    @Nested
+    @DisplayName("setting values to the authProvider configurations")
+    class ConfigurationsTest {
+        @Test
+        void testAuthProviderConfiguration() throws Exception {
             AuthProvider provider = new AuthProvider.Configurations()
                     .projectId("test-project")
                     .tokenId("test-token")
-                    .privateKey("invalid-key")
-                    .passphrase(phrase)
+                    .privateKey("test-key")
                     .build();
-            provider.fetchProjectScopedToken();
-        });
 
-        assertTrue(exception.getMessage().startsWith("Could not derive private key out of the configurations."));
+            assertNotNull(provider);
+            assertEquals("test-project", provider.getProjectId());
+            assertEquals("test-token", provider.getTokenId());
+            assertEquals("test-key", provider.getPrivateKey());
+        }
+    }
+
+    @Nested
+    @DisplayName("fetchProjectScopedToken method")
+    class FetchProjectScopedTokenTest {
+        @ParameterizedTest
+        @DisplayName("throws given an invalid private-key and a empty or non-empty passphrase")
+        @EmptySource
+        @ValueSource(strings = { "complicated-word" })
+        void givenInvalidPrivateKey_thenThrows(String phrase) {
+            Exception exception = assertThrows(Exception.class, () -> {
+                AuthProvider provider = new AuthProvider.Configurations()
+                        .projectId("test-project")
+                        .tokenId("test-token")
+                        .privateKey("invalid-key")
+                        .passphrase(phrase)
+                        .build();
+                provider.fetchProjectScopedToken();
+            });
+
+            assertTrue(exception.getMessage().startsWith("Could not derive private key out of the configurations."));
+        }
     }
 
     @Test
