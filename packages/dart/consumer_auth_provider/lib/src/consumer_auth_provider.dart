4864065afb8754868f4903c1fae7f954ccb210eb
Earl Grant Reyes <earl.r@affinidi.com>
2025-01-21 13:29:30 +0800

    chore: move docs from base implementation

--- packages/dart/consumer_auth_provider/lib/src/consumer_auth_provider.dart
+++ packages/dart/consumer_auth_provider/lib/src/consumer_auth_provider.dart
@@ -3,6 +3,18 @@ import 'consumer_auth_provider_abstract.dart';
 import 'consumer_auth_provider_interface.dart';
 
 class ConsumerAuthProvider implements ConsumerAuthProviderInterface {
+  /// A provider for handling Consumer authentication token
+  ///
+  /// Example usage:
+  ///```dart
+  ///void main() {
+  ///  final consumerAuthProvider = ConsumerAuthProvider(
+  ///    encryptedSeed: 'encryptedSeed',
+  ///    encryptionKey: 'encryptionKey',
+  ///  );
+  ///  final token = await consumerAuthProvider.fetchConsumerToken();
+  ///}
+  ///```
   ConsumerAuthProvider({
     required String encryptedSeed,
     required String encryptionKey,
@@ -13,6 +25,8 @@ class ConsumerAuthProvider implements ConsumerAuthProviderInterface {
     );
   }
 
+  /// Retrieves a valid Consumer token. Checks its validity and, if necessary,
+  /// sends a request to the server to obtain a fresh token.
   @override
   Future<String> fetchConsumerToken() async {
     return await ConsumerAuthProviderAbstract.instance.fetchConsumerToken();
