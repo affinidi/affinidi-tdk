94db23770b89e4c2d1d942daa748dde9f586a5da
Roman Brazhnyk <roman.b@affinidi.com>
2025-03-03 17:35:30 +0800

    feat: add iota-core library

---
+++ libs/dart/iota_core/README.md
@@ -0,0 +1,38 @@
+# Affinidi TDK - Iota core library
+
+The `affinidi_tdk_iota_core` package provides methods to ...
+
+## Table of Contents
+
+- [Affinidi TDK - Iota core](#affinidi-tdk---iota-core)
+  - [Table of Contents](#table-of-contents)
+  - [Requirements](#requirements)
+  - [Getting Started](#getting-started)
+  - [Usage](#usage)
+
+## Requirements
+
+- Dart SDK version ^3.6.0
+
+## Getting Started
+
+Add the following to your `pubspec.yaml` file:
+
+```yaml
+dependencies:
+  affinidi_tdk_iota_core: ^<version_number>
+```
+
+Then run:
+
+```bash
+dart pub get
+```
+
+## Usage
+
+Once you've installed the package, import it into your Dart code:
+
+```dart
+...
+```
---
+++ libs/dart/iota_core/example/main.dart
@@ -0,0 +1,41 @@
+import 'package:affinidi_tdk_auth_provider/affinidi_tdk_auth_provider.dart';
+import 'package:affinidi_tdk_iota_core/affinidi_tdk_iota_core.dart';
+import 'package:affinidi_tdk_common/affinidi_tdk_common.dart';
+import 'package:dotenv/dotenv.dart';
+
+void main() async {
+  var env = DotEnv()..load();
+  if (!env.isEveryDefined(['PROJECT_ID', 'TOKEN_ID', 'PRIVATE_KEY'])) {
+    print(
+        'Missing variables. Please provide PROJECT_ID, TOKEN_ID and PRIVATE_KEY');
+    return;
+  }
+  // Workaround for dotenv multiline limitations
+  final privateKey = env['PRIVATE_KEY']!.replaceAll('\\n', '\n');
+
+  final provider = AuthProvider(
+    projectId: env['PROJECT_ID']!,
+    tokenId: env['TOKEN_ID']!,
+    privateKey: privateKey,
+    // Optional parameters
+    keyId: env['KEY_ID'],
+    passphrase: env['PASSPHRASE'],
+  );
+
+  // Fetch iota token (websocket).
+  // DID is usually obtained at runtime from the registered user
+  if (!env.isEveryDefined(['IOTA_CONFIG_ID', 'DID'])) {
+    print('Missing variables. Please provide IOTA_CONFIG_ID and DID');
+    return;
+  }
+  try {
+    final iotaToken = provider.createIotaToken(
+        iotaConfigId: env['IOTA_CONFIG_ID']!, did: env['DID']!);
+
+    final iotaCredentials = await IotaCore.limitedTokenToIotaCredentials(iotaToken.iotaJwt);
+    print(iotaCredentials.credentials.accessKeyId);
+    print(iotaCredentials.connectionClientId);
+  } catch (e) {
+    print('Error obtaining token: $e');
+  }
+}
