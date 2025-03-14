b66fe7de159a3390dcdc3f73840c3e03c075a9ef
Marco A <marco.a@affinidi.com>
2025-03-14 16:33:54 +0100

    feat: allowed Dio injection via constructor to ConsumerAuthProvider

--- packages/dart/consumer_auth_provider/lib/src/provider/consumer_token_provider.dart
+++ packages/dart/consumer_auth_provider/lib/src/provider/consumer_token_provider.dart
@@ -3,7 +3,10 @@ part of 'token_provider.dart';
 /// Auth provider that generates consumer scoped tokens used by Vault to call
 /// Affinidi Vault services such as Vault Data Manager (VDM).
 class ConsumerTokenProvider extends TokenProvider {
+  ConsumerTokenProvider({Dio? client}) : _client = client ?? Dio();
+
   static final String _tokenEndpoint = Environment.fetchConsumerAudienceUrl();
+  final Dio _client;
 
   @override
   Future<String> getToken(Uint8List seed) async {
@@ -19,7 +22,6 @@ class ConsumerTokenProvider extends TokenProvider {
   }
 
   Future<String> _getConsumerToken(String clientAssertion, String did) async {
-    final dioInstance = Dio();
     final data = {
       "grant_type": 'client_credentials',
       "client_assertion_type":
@@ -28,7 +30,7 @@ class ConsumerTokenProvider extends TokenProvider {
       "client_id": did,
     };
 
-    final response = await dioInstance.post(
+    final response = await _client.post(
       _tokenEndpoint,
       data: data,
       options: Options(
