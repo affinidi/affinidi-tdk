af04d210d28f3670148eb8f80a06b00234b78700
Marat Shakirov <marat.s@affinidi.com>
2025-04-01 22:31:22 +0000

    feat: update all clients

---
+++ clients/dart/iam_client/test/grant_access_output_test.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+import 'package:affinidi_tdk_iam_client/affinidi_tdk_iam_client.dart';
+
+// tests for GrantAccessOutput
+void main() {
+  final instance = GrantAccessOutputBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(GrantAccessOutput, () {
+    // bool success
+    test('to test the property `success`', () async {
+      // TODO
+    });
+
+    // Unique identifier for the access grant
+    // String grantId
+    test('to test the property `grantId`', () async {
+      // TODO
+    });
+
+  });
+}
