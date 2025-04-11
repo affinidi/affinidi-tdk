45a81df680659b16b94985256453d01b3113401e
Affinidi Pte. Ltd <pub.dev@affinidi.com>
2025-04-11 11:41:28 +0000

    feat: update client cis

---
+++ clients/dart/credential_issuance_client/lib/src/model/cors_batch_credential_ok.g.dart
@@ -0,0 +1,97 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cors_batch_credential_ok.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+class _$CorsBatchCredentialOK extends CorsBatchCredentialOK {
+  @override
+  final String? corsBatchCredentialOk;
+
+  factory _$CorsBatchCredentialOK(
+          [void Function(CorsBatchCredentialOKBuilder)? updates]) =>
+      (new CorsBatchCredentialOKBuilder()..update(updates))._build();
+
+  _$CorsBatchCredentialOK._({this.corsBatchCredentialOk}) : super._();
+
+  @override
+  CorsBatchCredentialOK rebuild(
+          void Function(CorsBatchCredentialOKBuilder) updates) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  CorsBatchCredentialOKBuilder toBuilder() =>
+      new CorsBatchCredentialOKBuilder()..replace(this);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is CorsBatchCredentialOK &&
+        corsBatchCredentialOk == other.corsBatchCredentialOk;
+  }
+
+  @override
+  int get hashCode {
+    var _$hash = 0;
+    _$hash = $jc(_$hash, corsBatchCredentialOk.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper(r'CorsBatchCredentialOK')
+          ..add('corsBatchCredentialOk', corsBatchCredentialOk))
+        .toString();
+  }
+}
+
+class CorsBatchCredentialOKBuilder
+    implements Builder<CorsBatchCredentialOK, CorsBatchCredentialOKBuilder> {
+  _$CorsBatchCredentialOK? _$v;
+
+  String? _corsBatchCredentialOk;
+  String? get corsBatchCredentialOk => _$this._corsBatchCredentialOk;
+  set corsBatchCredentialOk(String? corsBatchCredentialOk) =>
+      _$this._corsBatchCredentialOk = corsBatchCredentialOk;
+
+  CorsBatchCredentialOKBuilder() {
+    CorsBatchCredentialOK._defaults(this);
+  }
+
+  CorsBatchCredentialOKBuilder get _$this {
+    final $v = _$v;
+    if ($v != null) {
+      _corsBatchCredentialOk = $v.corsBatchCredentialOk;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(CorsBatchCredentialOK other) {
+    ArgumentError.checkNotNull(other, 'other');
+    _$v = other as _$CorsBatchCredentialOK;
+  }
+
+  @override
+  void update(void Function(CorsBatchCredentialOKBuilder)? updates) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  CorsBatchCredentialOK build() => _build();
+
+  _$CorsBatchCredentialOK _build() {
+    final _$result = _$v ??
+        new _$CorsBatchCredentialOK._(
+          corsBatchCredentialOk: corsBatchCredentialOk,
+        );
+    replace(_$result);
+    return _$result;
+  }
+}
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
