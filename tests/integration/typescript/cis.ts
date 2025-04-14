89e99d1a1e4188b6ae9987b0b941005a1b68bc5c
Mohamed Abdelsamei <mohamed.e@affinidi.com>
2025-04-14 17:16:20 +0200

    feat: bc integration test typescript

--- tests/integration/typescript/cis.ts
+++ tests/integration/typescript/cis.ts
@@ -1,17 +1,113 @@
 import { expect } from 'chai'
-import { IssuanceApi, Configuration as CisConfiguration, StartIssuanceInput, StartIssuanceInputClaimModeEnum } from '@affinidi-tdk/credential-issuance-client'
-import { apiKey, projectId, credentialIssuanceData } from './helpers'
+import {
+  IssuanceApi,
+  Configuration as CisConfiguration,
+  StartIssuanceInput,
+  CredentialsApi,
+  BatchCredentialInput,
+  OfferApi,
+  BatchCredentialInputCredentialRequestsInner,
+} from '@affinidi-tdk/credential-issuance-client'
+import {
+  apiKey,
+  projectId,
+  credentialIssuanceData,
+  getCisToken,
+} from './helpers'
+import axios from 'axios'
 
 describe('credential-issuance-client', function () {
+  let credentialToken = 'token'
+  let offerUri = ''
+  let issuanceId = ''
+  let preAuthCode = ''
+  let credentialIssuer = ''
+  let authorizationDetails
+  let txCode = ''
   it('tests startIssuance', async () => {
-    const cisConfiguration = new CisConfiguration({ apiKey })
+    const cisConfiguration = new CisConfiguration({
+      apiKey,
+      basePath: 'https://apse1.dev.api.affinidi.io/cis',
+    })
     const api = new IssuanceApi(cisConfiguration)
 
     const request: StartIssuanceInput = JSON.parse(credentialIssuanceData)
 
     const { data } = await api.startIssuance(projectId, request)
-
+    offerUri = data.credentialOfferUri
+    issuanceId = data.issuanceId
     expect(data).to.have.a.property('credentialOfferUri')
     expect(data).to.have.a.property('issuanceId')
+  })
+
+  it('test offer Uri', async () => {
+    const cisConfiguration = new CisConfiguration({
+      apiKey,
+      basePath: 'https://apse1.dev.api.affinidi.io/cis',
+    })
+    const api = new OfferApi(cisConfiguration)
+    const { data } = await api.getCredentialOffer(projectId, issuanceId)
+
+    expect(data).to.have.a.property('grants')
+    expect(data).to.have.a.property('credential_issuer')
+    expect(data.grants).to.have.a.property(
+      'urn:ietf:params:oauth:grant-type:pre-authorized_code',
+    )
+    expect(
+      data.grants['urn:ietf:params:oauth:grant-type:pre-authorized_code'],
+    ).to.have.a.property('pre-authorized_code')
+    preAuthCode =
+      data.grants['urn:ietf:params:oauth:grant-type:pre-authorized_code'][
+        'pre-authorized_code'
+      ]
+    credentialIssuer = data.credential_issuer
+  })
+
+  it('Test fetch credential token', async () => {
+    const response = await axios.post(
+      `${credentialIssuer}/oauth2/token`,
+      {
+        grant_type: 'urn:ietf:params:oauth:grant-type:pre-authorized_code',
+        'pre-authorized_code': preAuthCode,
+        tx_code: txCode,
+      },
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      },
+    )
+
+    expect(response.status).to.equal(200)
+    credentialToken = response.data.access_token
+    authorizationDetails = response.data.authorization_details
+  })
+
+  it('tests batch credential', async () => {
+    const cisConfiguration = new CisConfiguration({
+      accessToken: `${credentialToken}`,
+      basePath: 'https://apse1.dev.api.affinidi.io/cis',
+    })
+    const api = new CredentialsApi(cisConfiguration)
+    const credentialRequests: Array<BatchCredentialInputCredentialRequestsInner> =
+      []
+    authorizationDetails.forEach((element: any) => {
+      element.credential_identifiers.forEach((credentialIdentifier) => {
+        const credentialRequest: BatchCredentialInputCredentialRequestsInner = {
+          credential_identifier: credentialIdentifier,
+          proof: {
+            proof_type: 'jwt',
+            jwt: getCisToken(),
+          },
+        }
+        credentialRequests.push(credentialRequest)
+      })
+    })
+
+    const request: BatchCredentialInput = {
+      credential_requests: credentialRequests,
+    }
+    const { data } = await api.batchCredential(projectId, request)
+    expect(data).to.have.a.property('credential_responses')
+    expect(data.credential_responses.length).to.equal(2)
+    expect(data.credential_responses[0]).to.have.a.property('credential')
   })
 })
--- tests/integration/typescript/helpers/index.ts
+++ tests/integration/typescript/helpers/index.ts
@@ -1,28 +1,39 @@
 require('dotenv').config({ path: '../../.env' })
 
 import { AuthProvider } from '@affinidi-tdk/auth-provider'
+import { AuthenticationService } from './authenticationService'
 
 const {
-  KEY_ID: keyId ='',
-  TOKEN_ID: tokenId ='',
-  PASSPHRASE: passphrase ='',
-  PRIVATE_KEY: privateKey ='',
-  PROJECT_ID: projectId ='',
-  VERIFIABLE_CREDENTIAL: verifiableCredential ='',
-  CREDENTIAL_ISSUANCE_DATA: credentialIssuanceData ='',
-  IOTA_CONFIG_ID: configurationId ='',
-  QUERY_ID: queryId ='',
-  DID: did ='',
-  REDIRECT_URI: redirectUri ='',
-  PRESENTATION_SUBMISSION: presentation_submission ='',
-  VP_TOKEN: vp_token ='',
-  UNSIGNED_CREDENTIAL: unsignedCredential ='',
-  UNSIGNED_CREDENTIAL_PARAMS: unsignedCredentialParams ='',
-  WALLET_ID: walletId ='',
+  KEY_ID: keyId = '',
+  TOKEN_ID: tokenId = '',
+  PASSPHRASE: passphrase = '',
+  PRIVATE_KEY: privateKey = '',
+  PROJECT_ID: projectId = '',
+  VERIFIABLE_CREDENTIAL: verifiableCredential = '',
+  CREDENTIAL_ISSUANCE_DATA: credentialIssuanceData = '',
+  IOTA_CONFIG_ID: configurationId = '',
+  QUERY_ID: queryId = '',
+  DID: did = '',
+  REDIRECT_URI: redirectUri = '',
+  PRESENTATION_SUBMISSION: presentation_submission = '',
+  VP_TOKEN: vp_token = '',
+  UNSIGNED_CREDENTIAL: unsignedCredential = '',
+  UNSIGNED_CREDENTIAL_PARAMS: unsignedCredentialParams = '',
+  WALLET_ID: walletId = '',
+  BATCH_CREDENTIAL_REQUEST: batchCredentialRequest = '',
+  // wallet
+  ENCRYPTION_SEED: encryptionSeed = '',
+  SEED_PASSWORD: seedPassword = '',
 } = process.env
 
 // 🍌 init AuthProvider
-const authProvider = new AuthProvider({ keyId, tokenId, passphrase, privateKey, projectId })
+const authProvider = new AuthProvider({
+  keyId,
+  tokenId,
+  passphrase,
+  privateKey,
+  projectId,
+})
 const apiKey = authProvider.fetchProjectScopedToken.bind(authProvider)
 
 export {
@@ -40,4 +51,16 @@ export {
   unsignedCredential,
   unsignedCredentialParams,
   walletId,
+  batchCredentialRequest,
+}
+
+
+export const  getCisToken = ()=>{
+  const authenticationService = new AuthenticationService()
+  const cisToken =  authenticationService.signAssertion(
+    encryptionSeed,
+    seedPassword,
+    'https://apse1.dev.api.affinidi.io/cis',
+  )
+  return cisToken
 }
