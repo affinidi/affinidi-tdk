729ea078a43da8fdbafa5646206846d91616e66c
Marat Shakirov <marat.s@affinidi.com>
2025-05-07 15:13:17 +0200

    fix: empty test

--- packages/dart/affinidi_tdk_test_utilities/test/affinidi_tdk_test_utilities_test.dart
+++ packages/dart/affinidi_tdk_test_utilities/test/affinidi_tdk_test_utilities_test.dart
@@ -2,7 +2,7 @@ import 'package:affinidi_tdk_test_utilities/affinidi_tdk_test_utilities.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('placeholder', () {
+    expect(true, isTrue);
   });
 }
