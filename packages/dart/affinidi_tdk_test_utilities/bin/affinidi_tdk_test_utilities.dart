09df38c150892b0bb0033eb4ceeeb751e2bed326
Marat Shakirov <marat.s@affinidi.com>
2025-05-07 13:36:39 +0200

    feat: create empty dart test utilities packages

---
+++ packages/dart/affinidi_tdk_test_utilities/bin/affinidi_tdk_test_utilities.dart
@@ -0,0 +1,5 @@
+import 'package:affinidi_tdk_test_utilities/affinidi_tdk_test_utilities.dart' as affinidi_tdk_test_utilities;
+
+void main(List<String> arguments) {
+  print('Hello world: ${affinidi_tdk_test_utilities.calculate()}!');
+}
---
+++ packages/dart/affinidi_tdk_test_utilities/test/affinidi_tdk_test_utilities_test.dart
@@ -0,0 +1,8 @@
+import 'package:affinidi_tdk_test_utilities/affinidi_tdk_test_utilities.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
