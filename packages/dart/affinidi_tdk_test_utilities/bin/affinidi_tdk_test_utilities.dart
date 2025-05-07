b5bd71595f8fa939beb86c38c5ecc440447b90ac
Marat Shakirov <marat.s@affinidi.com>
2025-05-07 13:59:46 +0200

    fix: remove bing

--- packages/dart/affinidi_tdk_test_utilities/bin/affinidi_tdk_test_utilities.dart
+++
@@ -1,5 +0,0 @@
-import 'package:affinidi_tdk_test_utilities/affinidi_tdk_test_utilities.dart' as affinidi_tdk_test_utilities;
-
-void main(List<String> arguments) {
-  print('Hello world: ${affinidi_tdk_test_utilities.calculate()}!');
-}
