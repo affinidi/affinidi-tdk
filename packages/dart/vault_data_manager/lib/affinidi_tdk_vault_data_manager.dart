a7ef1c2a8acc9947889491b3077bd4893ebbc5d6
Kostiantyn Synyshyn <kostiantyn.s@affinidi.com>
2025-05-21 19:29:24 +0300

    feat: add affinidi_api_cancel_token implementation

--- packages/dart/vault_data_manager/lib/affinidi_tdk_vault_data_manager.dart
+++ packages/dart/vault_data_manager/lib/affinidi_tdk_vault_data_manager.dart
@@ -2,6 +2,7 @@
 library;
 
 export 'src/exceptions/tdk_exception_type.dart';
+export 'src/helpers/affinidi_api_cancel_token.dart';
 export 'src/model/data_encryption_material.dart';
 export 'src/vault_data_manager_api_service.dart';
 export 'src/vault_data_manager_api_service_interface.dart';
