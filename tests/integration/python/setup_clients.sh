b77da10431aff346fbb12d1ec42170e6a063cc43
Roman Brazhnyk <roman.b@affinidi.com>
2025-06-02 11:25:30 +0800

    fix: install missing client in setup script for python. update workflow for dart

--- tests/integration/python/setup_clients.sh
+++ tests/integration/python/setup_clients.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Resolve the directory of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Set SDK root to be 3 levels up from this script location
+# Set TDK root to be 3 levels up from this script location
 TDK_ROOT="$(cd "$SCRIPT_DIR/../../../" && pwd)"
 
 # Activate virtualenv if applicable (optional)
@@ -14,5 +14,6 @@ TDK_ROOT="$(cd "$SCRIPT_DIR/../../../" && pwd)"
 pip install -e "$TDK_ROOT/clients/python/credential_issuance_client"
 pip install -e "$TDK_ROOT/clients/python/credential_verification_client"
 pip install -e "$TDK_ROOT/clients/python/iam_client"
+pip install -e "$TDK_ROOT/clients/python/iota_client"
 pip install -e "$TDK_ROOT/clients/python/login_configuration_client"
 pip install -e "$TDK_ROOT/clients/python/wallets_client"
