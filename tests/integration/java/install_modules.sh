144ee37f3cd8f4d5078d530c64c128514a2cb6fa
Roman Brazhnyk <roman.b@affinidi.com>
2025-06-03 16:09:09 +0800

    fix: logs

--- tests/integration/java/install_modules.sh
+++ tests/integration/java/install_modules.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing all Java clients..."
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "$SCRIPT_DIR/../../../" && pwd)"
 
-# Build and install auto-generated clients
-for client_dir in ../../../clients/java/*; do
+echo "SCRIPT_DIR ", $SCRIPT_DIR
+echo "REPO_ROOT ", $REPO_ROOT
+
+echo "Installing all TDK clients..."
+
+for client_dir in "$REPO_ROOT/clients/java"/*; do
   if [ -d "$client_dir" ]; then
     echo "Installing client in $client_dir"
     (cd "$client_dir" && mvn clean install -DskipTests)
@@ -13,8 +18,7 @@ done
 
 echo "Installing shared packages..."
 
-# Build and install shared packages
-for package_dir in ../../../packages/java/*; do
+for package_dir in "$REPO_ROOT/packages/java"/*; do
   if [ -d "$package_dir" ]; then
     echo "Installing package in $package_dir"
     (cd "$package_dir" && mvn clean install -DskipTests)
