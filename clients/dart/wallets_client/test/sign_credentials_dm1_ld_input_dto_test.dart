3105738fea4873d59cf5314822db1c0c7311833f
Affinidi Pte. Ltd. <pub.dev@affinidi.com>
2025-06-17 22:26:27 +0000

    feat: update all clients

---
+++ clients/dart/wallets_client/test/sign_credentials_dm1_ld_input_dto_test.dart
@@ -0,0 +1,22 @@
+import 'package:test/test.dart';
+import 'package:affinidi_tdk_wallets_client/affinidi_tdk_wallets_client.dart';
+
+// tests for SignCredentialsDm1LdInputDto
+void main() {
+  final instance = SignCredentialsDm1LdInputDtoBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(SignCredentialsDm1LdInputDto, () {
+    // Unsigned Credential in Dm1 format
+    // JsonObject unsignedCredential
+    test('to test the property `unsignedCredential`', () async {
+      // TODO
+    });
+
+    // bool revocable
+    test('to test the property `revocable`', () async {
+      // TODO
+    });
+
+  });
+}
---
+++ clients/java/wallets.client/docs/SignCredentialsDm2SdJwtInputDto.md
@@ -0,0 +1,11 @@
+# SignCredentialsDm2SdJwtInputDto
+
+DTO contains params to sign credential
+
+## Properties
+
+| Name                   | Type        | Description                       | Notes      |
+| ---------------------- | ----------- | --------------------------------- | ---------- |
+| **unsignedCredential** | **Object**  | Unsigned Credential in Dm2 format |            |
+| **revocable**          | **Boolean** |                                   | [optional] |
+| **disclosureFrame**    | **Object**  |                                   |            |
