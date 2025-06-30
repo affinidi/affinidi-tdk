a1be14e13b4bf2bf434cd2dce898d1a33084ddca
Marco Attanasio <marco.a@affinidi.com>
2025-06-30 09:32:57 +0200

    fix: remove direct dependency on flutter

--- packages/dart/vault_edge_drift_provider/lib/src/database/database_factory.dart
+++
@@ -1,2 +0,0 @@
-export 'database_factory_native.dart'
-    if (dart.library.html) 'database_factory_web.dart';
---
+++ packages/dart/vault_edge_drift_provider/lib/src/database/database_platform.dart
@@ -0,0 +1,2 @@
+export 'database_platform_native.dart'
+    if (dart.library.html) 'database_platform_web.dart';
