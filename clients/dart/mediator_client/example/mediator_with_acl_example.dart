4cb8e442b9f60891698e68c559c0d8db56122cb4
Ivan Branets <ivan.b@affinidi.com>
2025-08-10 20:01:40 +0200

    feat: didcomm mediator

---
+++ clients/dart/mediator_client/example/mediator_with_acl_example.dart
@@ -0,0 +1,251 @@
+import 'package:didcomm/src/mediator_client/mediator_client_exception.dart';
+import 'package:mediator_client/mediator_client.dart';
+import 'package:ssi/ssi.dart';
+import 'package:uuid/uuid.dart';
+
+import '../test/example_configs.dart';
+
+// TODO: Configure private keys and mediator DID on CI
+void main() async {
+  // Create and run a DIDComm mediator, for instance https://github.com/affinidi/affinidi-tdk-rs/tree/main/crates/affinidi-messaging/affinidi-messaging-mediator or with https://portal.affinidi.com.
+  // Configure ACL.
+  // Copy its DID Document URL into example/mediator/mediator_with_acl_did.txt.
+  final aliceKeyStore = InMemoryKeyStore();
+  final aliceWallet = PersistentWallet(aliceKeyStore);
+
+  final bobKeyStore = InMemoryKeyStore();
+  final bobWallet = PersistentWallet(bobKeyStore);
+
+  final aliceDidManager = DidKeyManager(
+    wallet: aliceWallet,
+    store: InMemoryDidStore(),
+  );
+
+  final bobDidManager = DidKeyManager(
+    wallet: bobWallet,
+    store: InMemoryDidStore(),
+  );
+
+  final aliceKeyId = 'alice-key-1';
+
+  await aliceWallet.generateKey(
+    keyId: aliceKeyId,
+    keyType: KeyType.p256,
+  );
+
+  await aliceDidManager.addVerificationMethod(aliceKeyId);
+  final aliceDidDocument = await aliceDidManager.getDidDocument();
+
+  prettyPrint('Alice DID', object: aliceDidDocument.id);
+
+  final aliceSigner = await aliceDidManager.getSigner(
+    aliceDidDocument.assertionMethod.first.id,
+  );
+
+  final bobKeyId = 'bob-key-1';
+
+  await bobWallet.generateKey(
+    keyId: bobKeyId,
+    keyType: KeyType.p256,
+  );
+
+  await bobDidManager.addVerificationMethod(bobKeyId);
+  final bobDidDocument = await bobDidManager.getDidDocument();
+
+  prettyPrint('Bob DID Document', object: bobDidDocument);
+
+  final bobMediatorDocument =
+      await UniversalDIDResolver.defaultResolver.resolveDid(
+    await readDid(mediatorWithAclDidPath),
+  );
+
+  prettyPrint('Bob Mediator Document', object: bobMediatorDocument);
+
+  final bobSigner = await bobDidManager.getSigner(
+    bobDidDocument.assertionMethod.first.id,
+  );
+
+  final alicePlainTextMassage = PlainTextMessage(
+    id: const Uuid().v4(),
+    from: aliceDidDocument.id,
+    to: [bobDidDocument.id],
+    type: Uri.parse('https://didcomm.org/example/1.0/message'),
+    body: {'content': 'Hello, Bob!'},
+  );
+
+  alicePlainTextMassage['custom-header'] = 'custom-value';
+
+  prettyPrint('Plain Text Message for Bob', object: alicePlainTextMassage);
+
+  final aliceSignedAndEncryptedMessage =
+      await DidcommMessage.packIntoSignedAndEncryptedMessages(
+    alicePlainTextMassage,
+    keyType: [bobDidDocument].getCommonKeyTypesInKeyAgreements().first,
+    recipientDidDocuments: [bobDidDocument],
+    keyWrappingAlgorithm: KeyWrappingAlgorithm.ecdhEs,
+    encryptionAlgorithm: EncryptionAlgorithm.a256cbc,
+    signer: aliceSigner,
+  );
+
+  prettyPrint(
+    'Encrypted and Signed Message by Alice',
+    object: aliceSignedAndEncryptedMessage,
+  );
+
+  final createdTime = DateTime.now().toUtc();
+  final expiresTime = createdTime.add(const Duration(seconds: 60));
+
+  final forwardMessage = ForwardMessage(
+    id: const Uuid().v4(),
+    from: aliceDidDocument.id,
+    to: [bobMediatorDocument.id],
+    next: bobDidDocument.id,
+    expiresTime: expiresTime,
+    attachments: [
+      Attachment(
+        mediaType: 'application/json',
+        data: AttachmentData(
+          base64: base64UrlEncodeNoPadding(
+            aliceSignedAndEncryptedMessage.toJsonBytes(),
+          ),
+        ),
+      ),
+    ],
+  );
+
+  prettyPrint(
+    'Forward Message for Mediator that wraps Encrypted Message for Bob',
+    object: forwardMessage,
+  );
+
+  final aliceMatchedKeyIds = aliceDidDocument.matchKeysInKeyAgreement(
+    otherDidDocuments: [bobMediatorDocument],
+  );
+
+  // Alice is going to use Bob's Mediator to send him a message
+  final aliceMediatorClient = MediatorClient(
+    mediatorDidDocument: bobMediatorDocument,
+    keyPair: await aliceDidManager.getKeyPairByDidKeyId(
+      aliceMatchedKeyIds.first,
+    ),
+    didKeyId: aliceMatchedKeyIds.first,
+    signer: aliceSigner,
+    forwardMessageOptions: const ForwardMessageOptions(
+      shouldSign: true,
+      shouldEncrypt: true,
+      keyWrappingAlgorithm: KeyWrappingAlgorithm.ecdhEs,
+      encryptionAlgorithm: EncryptionAlgorithm.a256cbc,
+    ),
+  );
+
+  // authenticate method is not direct part of mediatorClient, but it is extension method
+  // this method is need for mediators, that require authentication like an Affinidi mediator
+  final aliceTokens = await aliceMediatorClient.authenticate();
+
+  // Alice shall not send a message
+  try {
+    await aliceMediatorClient.sendMessage(
+      forwardMessage,
+      accessToken: aliceTokens.accessToken,
+    );
+    throw Exception(
+        'No error, Alice did send a message, are we using a mediator with explicit allow per DID?');
+  } on MediatorClientException catch (e) {
+    if (!e.innerMessage
+        .contains('Delivery blocked due to ACLs (access_list denied)')) {
+      throw Exception('Unexpected error occurred: ${e.innerMessage}');
+    }
+    prettyPrint('Expected error occurred', object: e.innerMessage);
+  } catch (e) {
+    throw Exception('Unexpected error occurred: $e');
+  }
+
+  final bobMatchedDidKeyIds = bobDidDocument.matchKeysInKeyAgreement(
+    otherDidDocuments: [
+      bobMediatorDocument,
+      // bob only sends messages to the mediator, so we don't need to match keys with Alice's DID Document
+    ],
+  );
+
+  final bobMediatorClient = MediatorClient(
+    mediatorDidDocument: bobMediatorDocument,
+    keyPair: await bobDidManager.getKeyPairByDidKeyId(
+      bobMatchedDidKeyIds.first,
+    ),
+    didKeyId: bobMatchedDidKeyIds.first,
+    signer: bobSigner,
+    forwardMessageOptions: const ForwardMessageOptions(
+      shouldSign: true,
+      shouldEncrypt: true,
+      keyWrappingAlgorithm: KeyWrappingAlgorithm.ecdhEs,
+      encryptionAlgorithm: EncryptionAlgorithm.a256cbc,
+    ),
+  );
+
+  final bobTokens = await bobMediatorClient.authenticate();
+
+  // Bob needs to add Alice's DID to their ACL...
+  final bobAccessListAddMessage = AccessListAddMessage(
+    id: const Uuid().v4(),
+    from: bobDidDocument.id,
+    to: [bobMediatorDocument.id],
+    theirDids: [aliceDidDocument.id],
+    expiresTime: expiresTime,
+  );
+
+  prettyPrint('bobAccessListAddMessage', object: bobAccessListAddMessage);
+
+  final bobAccessListAddSentMessage =
+      await bobMediatorClient.sendAclManagementMessage(
+    bobAccessListAddMessage,
+    accessToken: bobTokens.accessToken,
+  );
+
+  prettyPrint(
+    'bobAccessListAddSentMessage',
+    object: bobAccessListAddSentMessage,
+  );
+
+  // ...only then Alice can send a message
+  final sentMessage = await aliceMediatorClient.sendMessage(
+    forwardMessage,
+    accessToken: aliceTokens.accessToken,
+  );
+
+  prettyPrint('Encrypted and Signed Forward Message', object: sentMessage);
+
+  prettyPrint('Bob is fetching messages...');
+
+  final messageIds = await bobMediatorClient.listInboxMessageIds(
+    accessToken: bobTokens.accessToken,
+  );
+
+  final messages = await bobMediatorClient.receiveMessages(
+    messageIds: messageIds,
+    deleteOnMediator: true,
+    accessToken: bobTokens.accessToken,
+  );
+
+  for (final message in messages) {
+    prettyPrint(
+      'Raw message received by Bob via Mediator',
+      object: message,
+    );
+
+    final originalPlainTextMessage =
+        await DidcommMessage.unpackToPlainTextMessage(
+      message: message,
+      recipientDidManager: bobDidManager,
+      expectedMessageWrappingTypes: [
+        MessageWrappingType.anoncryptSignPlaintext,
+        MessageWrappingType.authcryptPlaintext,
+        MessageWrappingType.authcryptSignPlaintext,
+      ],
+    );
+
+    prettyPrint(
+      'Unpacked Plain Text Message received by Bob via Mediator',
+      object: originalPlainTextMessage,
+    );
+  }
+}
---
+++ clients/dart/mediator_client/lib/src/extensions/acl_management_extension.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:didcomm/didcomm.dart';
+import 'package:dio/dio.dart';
+
+// TODO: export correctly in DIDComm
+import 'package:didcomm/src/common/did_document_service_type.dart';
+import 'package:didcomm/src/mediator_client/mediator_client_exception.dart';
+
+// TODO: move this into messages folder to keep one class per file
+/// Represents a root DIDComm ACL Management Message.
+///
+/// This message is used as a parent to all ACL Management messages.
+abstract class AclManagementMessage extends PlainTextMessage {
+  /// Constructs [AclManagementMessage].
+  /// [id]: Unique message identifier.
+  /// [from]: Sender's DID.
+  /// [to]: List of recipient DIDs.
+  /// [body]: Message body.
+  /// [expiresTime]: Message expiration time (optional).
+  AclManagementMessage({
+    required super.id,
+    required super.from,
+    required super.to,
+    required super.body,
+    super.expiresTime,
+  }) : super(
+          type: Uri.parse('https://didcomm.org/mediator/1.0/acl-management'),
+        );
+}
+
+/// Represents a root DIDComm ACL Management Message.
+///
+/// This message is used to add DIDs to the mediator's ACL of the did (from).
+class AccessListAddMessage extends AclManagementMessage {
+  /// Lists of DIDs which shall be added to ACL.
+  final List<String> theirDids;
+
+  /// Constructs [AccessListAddMessage].
+  /// [id]: Unique message identifier.
+  /// [from]: Sender's DID.
+  /// [to]: List of recipient DIDs .
+  /// [theirDids]: Lists of DIDs which shall be added to ACL.
+  /// [expiresTime]: Message expiration time (optional).
+  AccessListAddMessage({
+    required super.id,
+    required super.from,
+    required super.to,
+    required this.theirDids,
+    super.expiresTime,
+  }) : super(
+          body: {
+            'access_list_add': {
+              'did_hash': sha256.convert(utf8.encode(from!)).toString(),
+              'hashes': theirDids
+                  .map((did) => sha256.convert(utf8.encode(did)).toString())
+                  .toList(),
+            }
+          },
+        );
+
+  /// Creates a [AccessListAddMessage] from a JSON map.
+  ///
+  /// [json]: The JSON map representing the message.
+  factory AccessListAddMessage.fromJson(Map<String, dynamic> json) {
+    final plainTextMessage = AccessListAddMessage.fromJson(json);
+    final accessList =
+        plainTextMessage.body?['access_list_add'] as Map<String, dynamic>?;
+
+    return AccessListAddMessage(
+      id: plainTextMessage.id,
+      from: plainTextMessage.from,
+      to: plainTextMessage.to,
+      theirDids: accessList?['hashes'] as List<String>,
+    );
+  }
+}
+
+// TODO: should be eventually moved to TDK
+/// Extension for [MediatorClient] to support Affinidi-specific ACL management.
+///
+/// ACL management is not covered by the DIDComm standard.
+/// This extension provides a method to configure ACLs with an Affinidi mediator.
+extension AffinidiAclManagementExtension on MediatorClient {
+  /// Sends a [AclManagementMessage] to the mediator.
+  ///
+  /// [message] - The message to send.
+  /// [accessToken] - Optional bearer token for authentication.
+  ///
+  /// Returns the packed [DidcommMessage] that was sent.
+  Future<DidcommMessage> sendAclManagementMessage(
+    AclManagementMessage message, {
+    String? accessToken,
+  }) async {
+    final dio = mediatorDidDocument.toDio(
+      mediatorServiceType: DidDocumentServiceType.didCommMessaging,
+    );
+
+    // TODO: remove internal in DIDComm
+    final messageToSend = await packMessage(
+      message,
+      messageOptions: forwardMessageOptions,
+    );
+
+    final headers =
+        accessToken != null ? {'Authorization': 'Bearer $accessToken'} : null;
+
+    try {
+      await dio.post<Map<String, dynamic>>(
+        '/inbound',
+        data: messageToSend,
+        options: Options(headers: headers),
+      );
+
+      return messageToSend;
+    } on DioException catch (error) {
+      throw MediatorClientException(innerException: error);
+    }
+  }
+}
