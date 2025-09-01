3da76eea7b0252a0859fd08157ac54b93d038693
Ivan Branets <ivan.b@affinidi.com>
2025-09-02 01:21:59 +0200

    chore: roll back to use dart pub get

--- tests/integration/dart/test/example_runner_test.dart
+++ tests/integration/dart/test/example_runner_test.dart
@@ -14,6 +14,19 @@ void main() async {
           config = await TestConfig.configureTestFiles(
             packageDirectoryName: 'mediator_client',
           );
+
+          final result = await Process.run(
+            Platform.resolvedExecutable,
+            ['pub', 'get'],
+            runInShell: true,
+            workingDirectory: config.packagePath,
+          );
+
+          if (result.exitCode != 0) {
+            throw Exception(
+              'Could not run "dart pub get".\nExit code: ${result.exitCode}.\nStdout: ${result.stdout}.\nStderr: ${result.stderr}.',
+            );
+          }
         });
 
         test(
