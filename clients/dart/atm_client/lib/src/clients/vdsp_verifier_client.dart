85aa0214e79199d66b452cf6e342934fbad9ba2f
Soroush Yousefpour <soroush.y@affinidi.com>
2025-09-18 14:52:23 +0200

    feat: implement VDSP discover-features query in verifier client

--- clients/dart/atm_client/lib/src/clients/vdsp_verifier_client.dart
+++ clients/dart/atm_client/lib/src/clients/vdsp_verifier_client.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:affinidi_tdk_mediator_client/mediator_client.dart';
-import 'package:ssi/src/did/did_manager/did_peer_manager.dart';
 import 'package:ssi/ssi.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../atm_client.dart';
 import '../common/atm_mediator_client.dart';
@@ -28,7 +28,79 @@ class VdspVerifierClient {
     required String holderDid,
     required String accessToken,
   }) async {
-    throw UnimplementedError();
+    final holderDidDocument =
+        await UniversalDIDResolver.defaultResolver.resolveDid(holderDid);
+
+    final queryMessage = PlainTextMessage(
+      id: const Uuid().v4(),
+      type: Uri.parse('https://didcomm.org/discover-features/2.0/queries'),
+      from: mediatorClient.signer.did,
+      to: [holderDidDocument.id],
+      body: {
+        'queries': [
+          {
+            'feature-type': 'protocol',
+            'match': 'https://affinidi.com/didcomm/protocols/vdsp/1.*',
+          },
+          {
+            'feature-type': 'operation',
+            'match': '*',
+          },
+          {
+            'feature-type': 'data_query_lang',
+            'match': '*',
+          },
+          {
+            'feature-type': 'vc_type',
+            'match': '*',
+          },
+          {
+            'feature-type': 'data_integrity_proof_suite',
+            'match': '*',
+          },
+          {
+            'feature-type': 'json_web_signature_algorithm',
+            'match': '*',
+          },
+        ],
+      },
+    );
+
+    final packagedMessageForHolder =
+        await DidcommMessage.packIntoSignedAndEncryptedMessages(
+      queryMessage,
+      recipientDidDocuments: [holderDidDocument],
+      keyWrappingAlgorithm: KeyWrappingAlgorithm.ecdh1Pu,
+      encryptionAlgorithm: EncryptionAlgorithm.a256cbc,
+      keyPair: mediatorClient.keyPair,
+      didKeyId: mediatorClient.didKeyId,
+      signer: mediatorClient.signer,
+    );
+
+    final createdTime = DateTime.now().toUtc();
+    final expiresTime = createdTime.add(clientOptions.messageExpiration);
+
+    final forwardMessage = ForwardMessage(
+      id: const Uuid().v4(),
+      to: [mediatorClient.mediatorDidDocument.id],
+      next: holderDidDocument.id,
+      expiresTime: expiresTime,
+      attachments: [
+        Attachment(
+          mediaType: 'application/json',
+          data: AttachmentData(
+            base64: base64UrlEncodeNoPadding(
+              packagedMessageForHolder.toJsonBytes(),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    await mediatorClient.sendMessage(
+      forwardMessage,
+      accessToken: accessToken,
+    );
   }
 
   Future<StreamSubscription<PlainTextMessage>> listenForDiscloseFeatures({
