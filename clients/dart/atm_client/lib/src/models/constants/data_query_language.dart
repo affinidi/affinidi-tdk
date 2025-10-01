2aa1f817c3eebc9fe49f600c37ed590afdf429b0
Ivan Branets <ivan.b@affinidi.com>
2025-10-01 08:06:48 +0200

    chore: remove redudand files

--- clients/dart/atm_client/lib/src/models/constants/data_query_language.dart
+++
@@ -1,9 +0,0 @@
-import 'package:json_annotation/json_annotation.dart';
-
-enum DataQueryLanguage {
-  @JsonValue('DCQL')
-  dcql('DCQL');
-
-  final String value;
-  const DataQueryLanguage(this.value);
-}
