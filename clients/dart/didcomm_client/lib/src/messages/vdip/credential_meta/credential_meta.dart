decfea688376e6b0b959a9345536355cc72d4ff1
kostiantyn.s <kostiantyn.s@affinidi.com>
2025-10-20 16:40:50 +0300

    feat: update credential_meta Dart doc

--- clients/dart/didcomm_client/lib/src/messages/vdip/credential_meta/credential_meta.dart
+++ clients/dart/didcomm_client/lib/src/messages/vdip/credential_meta/credential_meta.dart
@@ -4,18 +4,24 @@ part 'credential_meta.g.dart';
 
 @JsonSerializable(includeIfNull: false, explicitToJson: true)
 class CredentialMeta {
+  /// The context of the credential. E.g. [https://www.w3.org/2018/credentials/v1]
   @JsonKey(name: '@context')
   List<String>? context;
 
+  /// The did of the issuer. E.g. did:peer:issuer.usecase.com#key-4
   String? issuer;
 
+  /// Date the credential is valid from
   @JsonKey(name: 'valid_from')
   DateTime? validFrom;
 
+  /// Constructor
   CredentialMeta({this.context, this.issuer, this.validFrom});
 
+  /// Creates [CredentialMeta] from JSON.
   factory CredentialMeta.fromJson(Map<String, dynamic> json) =>
       _$CredentialMetaFromJson(json);
 
+  /// Converts [CredentialMeta] to JSON.
   Map<String, dynamic> toJson() => _$CredentialMetaToJson(this);
 }
