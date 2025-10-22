ee51304462549787893106b6ce32cdd156c17404
Kositantyn Synyshyn <kostiantyn.s@affinidi.com>
2025-10-22 19:57:07 +0300

    Feat/vdip message (#1031)
    * feat: add basic models
    
    * feat: add adapter
    
    * feat: implement JWT helper
    
    * feat: add jwt helper test
    
    * feat: implement request_credential_params to account for different scenarios
    
    * feat: update request_credential_params
    
    * feat: update credential_meta Dart doc
    
    * feat: update vdip_holder_client
    
    * feat: update exports
    
    * feat: update doc
    
    * feat: update example and client
    
    * feat: update jwt_helper implementation
    
    * feat: update jwt_helper tests
    
    * feat: update implementation after review
    
    * feat: update jwt_helper implementation
    
    * feat: update implementation
    
    * feat: add vdip_issued_credential_message and its body
    
    * feat: update imports

---
+++ clients/dart/didcomm_client/lib/src/messages/vdip/credential_meta/credential_meta.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'credential_meta.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CredentialMeta _$CredentialMetaFromJson(Map<String, dynamic> json) =>
+    CredentialMeta(
+      context: (json['@context'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      issuer: json['issuer'] as String?,
+      validFrom: json['valid_from'] == null
+          ? null
+          : DateTime.parse(json['valid_from'] as String),
+    );
+
+Map<String, dynamic> _$CredentialMetaToJson(CredentialMeta instance) =>
+    <String, dynamic>{
+      if (instance.context case final value?) '@context': value,
+      if (instance.issuer case final value?) 'issuer': value,
+      if (instance.validFrom?.toIso8601String() case final value?)
+        'valid_from': value,
+    };
