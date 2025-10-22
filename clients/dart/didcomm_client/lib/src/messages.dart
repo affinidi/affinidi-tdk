a3b89b8374e5967259777e283ab0ff37e8310172
kostiantyn.s <kostiantyn.s@affinidi.com>
2025-10-22 19:54:42 +0300

    feat: update imports

--- clients/dart/didcomm_client/lib/src/messages.dart
+++ clients/dart/didcomm_client/lib/src/messages.dart
@@ -6,3 +6,10 @@ export 'messages/ama/get_mediator_instances_list_message.dart';
 export 'messages/ama/get_mediator_requests_message.dart';
 export 'messages/ama/update_mediator_instance_configuration_message.dart';
 export 'messages/ama/update_mediator_instance_deployment_message.dart';
+
+// VDIP message exports
+export 'messages/vdip/credential_meta/credential_meta.dart';
+export 'messages/vdip/issued_credential/issued_credential_body/vdip_issued_credential_body.dart';
+export 'messages/vdip/issued_credential/issued_credential_message/vdip_issued_credential_message.dart';
+export 'messages/vdip/request_issuance/request_issuance_body/vdip_request_issuance_body.dart';
+export 'messages/vdip/request_issuance/request_issuance_message/vdip_request_issuance_message.dart';
