63152b3762e9cddbe980a96057aaaf69521a97b3
Ivan Branets <ivan.b@affinidi.com>
2025-10-30 10:51:19 +0100

    chore: add doc comments

--- clients/dart/didcomm/mediator_client/lib/src/models/constants/data_integrity_proof_suite.dart
+++ clients/dart/didcomm/mediator_client/lib/src/models/constants/data_integrity_proof_suite.dart
@@ -1,17 +1,28 @@
 import 'package:json_annotation/json_annotation.dart';
 
+/// Represents data integrity proof suites for verifiable credentials.
 enum DataIntegrityProofSuite {
+  /// SECP256K1 signature suite (2019)
   @JsonValue('secp256k1-signature-2019')
   secp256k1_signature_2019('secp256k1-signature-2019'),
+
+  /// ECDSA RDFC signature suite (2019)
   @JsonValue('ecdsa-rdfc-2019')
   ecdsa_rdfc_2019('ecdsa-rdfc-2019'),
+
+  /// ECDSA JCS signature suite (2019)
   @JsonValue('ecdsa-jcs-2019')
   ecdsa_jcs_2019('ecdsa-jcs-2019'),
+
+  /// EdDSA RDFC signature suite (2022)
   @JsonValue('eddsa-rdfc-2022')
   eddsa_rdfc_2022('eddsa-rdfc-2022'),
+
+  /// EdDSA JCS signature suite (2022)
   @JsonValue('eddsa-jcs-2022')
   eddsa_jcs_2022('eddsa-jcs-2022');
 
+  /// The string value of the proof suite
   final String value;
   const DataIntegrityProofSuite(this.value);
 }
--- clients/dart/didcomm/mediator_client/lib/src/models/constants/json_web_signature_algorithm.dart
+++ clients/dart/didcomm/mediator_client/lib/src/models/constants/json_web_signature_algorithm.dart
@@ -1,17 +1,28 @@
 import 'package:json_annotation/json_annotation.dart';
 
+/// Represents JSON Web Signature (JWS) algorithms for signing credentials.
 enum JsonWebSignatureAlgorithm {
+  /// ECDSA using P-256 curve and SHA-256
   @JsonValue('ES256')
   ES256('ES256'),
+
+  /// ECDSA using P-384 curve and SHA-384
   @JsonValue('ES384')
   ES384('ES384'),
+
+  /// ECDSA using P-521 curve and SHA-512
   @JsonValue('ES512')
   ES512('ES512'),
+
+  /// ECDSA using secp256k1 curve and SHA-256
   @JsonValue('ES256K')
   ES256K('ES256K'),
+
+  /// EdDSA signature algorithm
   @JsonValue('EdDSA')
   EdDSA('EdDSA');
 
+  /// The string value of the signature algorithm
   final String value;
   const JsonWebSignatureAlgorithm(this.value);
 }
