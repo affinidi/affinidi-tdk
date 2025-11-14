5305126851adbe74c23d40a3504b93b20d17fb99
Ivan Branets <ivan.b@affinidi.com>
2025-11-14 14:18:50 +0100

    fix: example runner

--- tests/integration/dart/test/example_runner_test.dart
+++ tests/integration/dart/test/example_runner_test.dart
@@ -12,7 +12,7 @@ void main() async {
       'didcomm_mediator_client',
       'vdsp',
       'vdip',
-      'atm_client'
+      'atm_client',
     ]) {
       group(packageName, () {
         late TestConfig config;
@@ -20,6 +20,8 @@ void main() async {
         setUp(() async {
           config = await TestConfig.configureTestFiles(
             packageDirectoryName: packageName,
+            skipBob: true,
+            skipMediator: true,
           );
 
           final result = await Process.run(
